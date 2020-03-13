a05c53fe1f68c0f079488a3ec36f683378db0873
Mohammed Saud <md.saud020@gmail.com>
2020-03-13 21:50:42 +0530

    use args iter directly in config constructor

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("No query string provided")
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("No filename provided")
+        };
 
         let case_sensitive = env::var("CASE_SENSITIVE").is_err();
 
@@ -88,6 +93,6 @@ Trust me.";
         assert_eq!(
             vec!["Rust:", "Trust me."],
             search_case_insensitive(query, contents)
-        );
+            );
     }
 }
